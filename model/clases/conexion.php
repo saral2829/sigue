6c0dd4dc41457f4c8a9a0c228f8517feb6cfe0a6
Brayian <bramirezag@gmail.com>
2022-10-07 13:29:06 -0500

    editando form producto

--- model/clases/conexion.php
+++ model/clases/conexion.php
@@ -1,18 +1,20 @@
 <?php
 
-class Conexion extends PDO {
-    
+class Conexion extends PDO
+{
+
     private $type = 'pgsql';
     private $host = 'localhost';
     private $port = '5432';
     private $db = 'bd_sigue';
     private $user = 'postgres';
-    private $pass = 'jsic@123'; //! Cambiar a la que estaba antes
+    private $pass = 'sarybell28'; //! Cambiar a la que estaba antes
 
-    public function __construct() {
-       
+    public function __construct()
+    {
+
         try {
-            parent::__construct($this->type . ':host=' . $this->host . ';port='.$this->port.';dbname=' . $this->db, $this->user, $this->pass);
+            parent::__construct($this->type . ':host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db, $this->user, $this->pass);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
@@ -20,4 +22,3 @@ class Conexion extends PDO {
         }
     }
 }
-?>
